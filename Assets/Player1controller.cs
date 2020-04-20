755132602cae5acb47204169cc01dbd0eccb7432
Mehak Gagneja <55054817+mehakgagneja02@users.noreply.github.com>
2020-04-20 14:10:41 +0530

    Add files via upload

---
+++ Assets/Player1controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player1controller : MonoBehaviour
+{
+    public KeyCode A = KeyCode.A;
+    public KeyCode D = KeyCode.D;
+    public KeyCode W = KeyCode.W;
+    public KeyCode S = KeyCode.S;
+    public int speed;
+    public Rigidbody rb;
+    public int count;
+    public Text Count;
+    public Text WinText;
+    private void Start()
+    {
+        count = 0;
+       WinText.text = "";
+
+
+    }
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(A))
+            rb.AddForce(Vector3.left * speed);
+        if (Input.GetKey(D))
+            rb.AddForce(Vector3.right * speed);
+        if (Input.GetKey(W))
+            rb.AddForce(Vector3.forward * speed);
+        if (Input.GetKey(S))
+            rb.AddForce(Vector3.back*speed);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Collect"))
+        {
+            other.gameObject.SetActive(false);
+            count =count + 1;
+            SetCount();
+        }
+    }
+    void SetCount()
+    {
+        Count.text = "Player 1: " + count.ToString();
+        if (count >= 6)
+        {
+            WinText.text = "Player 1 Wins";
+        }
+        
+    }
+}
---
+++ Assets/Player2controller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player2controller : MonoBehaviour
+{
+    public KeyCode left = KeyCode.LeftArrow;
+    public KeyCode right = KeyCode.RightArrow;
+    public KeyCode forward = KeyCode.UpArrow;
+    public KeyCode back = KeyCode.DownArrow;
+    public int speed;
+    public Rigidbody rb;
+    public int count1;
+    public Text Count1;
+    public Text WinText1;
+    private void Start()
+    {
+        count1 = 0;
+         WinText1.text = "";
+    }
+    private void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(left))
+            rb.AddForce(Vector3.left * speed);
+        if (Input.GetKey(right))
+            rb.AddForce(Vector3.right * speed);
+        if (Input.GetKey(forward))
+            rb.AddForce(Vector3.forward * speed);
+        if (Input.GetKey(back))
+            rb.AddForce(Vector3.back * speed);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collect"))
+        {
+            other.gameObject.SetActive(false);
+            count1 = count1 + 1;
+            SetCount1();
+        }
+    }
+    void SetCount1()
+    {
+        Count1.text = "Player 2: " + count1.ToString();
+        if (count1 >= 6)
+        {
+            WinText1.text = "Player 2 Wins";
+        }
+        
+    }
+}
